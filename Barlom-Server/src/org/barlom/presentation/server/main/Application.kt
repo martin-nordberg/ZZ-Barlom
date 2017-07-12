a048b9b1266ea777bc18987375c2ee4dc20d7fab
martin-nordberg <martin.nordberg@verizon.net>
2017-07-12 08:40:14 -0400

    * Added spark-kotlin dependency and hellow world web app.

--- Barlom-Server/src/org/barlom/presentation/server/main/Application.kt
+++ Barlom-Server/src/org/barlom/presentation/server/main/Application.kt
@@ -5,9 +5,27 @@
 
 package org.barlom.presentation.server.main
 
+import spark.kotlin.get
+import spark.kotlin.stop
+
 /**
  * Main program for Barlom Server.
  */
-fun main( args: Array<String> ) {
-    println( "Hello World!" )
+fun main(args: Array<String>) {
+
+    println("Server starting ...")
+
+    get("/hello") {
+        "Hello World"
+    }
+
+    get( "/bye" ) {
+        "Goodbye"
+    }
+
+    get( "/stop" ) {
+        stop()
+        "Server stopped"
+    }
+
 }
