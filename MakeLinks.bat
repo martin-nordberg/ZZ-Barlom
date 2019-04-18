fa95d73b675025018a9074ac5fe5ec37a20446e0
martin-nordberg <martin.nordberg@verizon.net>
2019-04-17 21:14:46 -0400

    Changed to using symlinks for Katydid dependencies.

---
+++ MakeLinks.bat
@@ -0,0 +1,13 @@
+
+rmdir Katydid-VDOM-JS
+mklink /d Katydid-VDOM-JS ..\Katydid\Katydid-VDOM-JS
+
+
+rmdir Katydid-CSS-JS
+mklink /d Katydid-CSS-JS ..\Katydid\Katydid-CSS-JS
+
+
+rmdir Katydid-Events-JS
+mklink /d Katydid-Events-JS ..\Katydid\Katydid-Events-JS
+
+
