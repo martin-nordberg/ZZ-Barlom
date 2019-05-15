50b0a2b98e38563a4f4e3286ca89273f03fa14d9
martin-nordberg <martin.nordberg@verizon.net>
2019-05-14 22:16:42 -0400

    Revised symlinks.

--- MakeLinks.bat
+++ MakeLinks.bat
@@ -11,3 +11,21 @@ rmdir Katydid-Events-JS
 mklink /d Katydid-Events-JS ..\Katydid\Katydid-Events-JS
 
 
+
+rmdir Barlom-Foundation-JS\src\main\kotlin\o
+mklink /d Barlom-Foundation-JS\src\main\kotlin\o ..\..\..\..\Barlom-Foundation-JVM\src\main\kotlin\o
+
+rmdir Barlom-Foundation-JS\src\main\kotlin\i
+mklink /d Barlom-Foundation-JS\src\main\kotlin\i ..\..\..\..\Barlom-Foundation-JVM\src\main\kotlin\i
+
+
+rmdir Barlom-Domain-JS\src\main\kotlin\o
+mklink /d Barlom-Domain-JS\src\main\kotlin\o ..\..\..\..\Barlom-Domain-JVM\src\main\kotlin\o
+
+
+rmdir Barlom-Client-JVM\src\main\kotlin\o
+mklink /d Barlom-Client-JVM\src\main\kotlin\o ..\..\..\..\Barlom-client-JS\src\main\kotlin\o
+
+
+
+
